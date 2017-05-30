9cba085c7f0a82bbb8e3b7d3c3e7a00f576c228c
Liam44 <liam-b@orange.fr>
2017-05-30 12:05:05 +0200

    Parking spots DropDownList

---
+++ MVCGarage/Models/EnumHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace MVCGarage.Models
+{
+    public static class EnumHelper
+    {
+        public static string GetDescriptionAttr(ETypeVehicle source)
+        {
+            FieldInfo fi = source.GetType().GetField(source.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0)
+                return attributes[0].Description;
+            else
+                return source.ToString();
+        }
+
+        public static IEnumerable<SelectListItem> PopulateDropList()
+        {
+            return Enum.GetValues(typeof(ETypeVehicle))
+                        .Cast<ETypeVehicle>()
+                        .Where(e => e != ETypeVehicle.undefined)
+                        .Select(e => new SelectListItem
+                        {
+                            Value = ((int)e).ToString(),
+                            Text = GetDescriptionAttr(e)
+                        });
+        }
+    }
+}
