83feecb986ab30c53908a46747a2e9dac88bbad8
Liam44 <liam-b@orange.fr>
2017-05-30 13:22:38 +0200

    Bug fixing in the GetDescriptionAttr

--- MVCGarage/Models/EnumHelper.cs
+++ MVCGarage/Models/EnumHelper.cs
@@ -9,17 +9,15 @@ namespace MVCGarage.Models
 {
     public static class EnumHelper
     {
-        public static string GetDescriptionAttr(ETypeVehicle source)
+        public static string GetDescriptionAttr<T>(this T source)
         {
             FieldInfo fi = source.GetType().GetField(source.ToString());
 
             DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(
                 typeof(DescriptionAttribute), false);
 
-            if (attributes != null && attributes.Length > 0)
-                return attributes[0].Description;
-            else
-                return source.ToString();
+            if (attributes != null && attributes.Length > 0) return attributes[0].Description;
+            else return source.ToString();
         }
 
         public static IEnumerable<SelectListItem> PopulateDropList()
