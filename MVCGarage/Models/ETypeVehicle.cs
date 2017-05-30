203705c119c4f1fee956bce1569b7ec04c7ccf00
Wille Hansson <wille.multi@live.se>
2017-05-30 09:59:05 +0200

    Vehicle, PS, EType classes

---
+++ MVCGarage/Models/ETypeVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGarage.Models
+{
+    public enum ETypeVehicle
+    {
+        car,
+        motorcycle,
+        bus,
+        truck
+    }
+}
