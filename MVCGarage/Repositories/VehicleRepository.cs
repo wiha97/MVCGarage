f9186b118c9bb0511cb1c72fd5c17e1636a22353
Wille Hansson <wille.multi@live.se>
2017-05-30 13:51:03 +0200

    VR

--- MVCGarage/Repositories/VehicleRepository.cs
+++ MVCGarage/Repositories/VehicleRepository.cs
@@ -17,9 +17,51 @@ namespace MVCGarage.Repositories
             return db.Vehicles;
         }
 
+        public Vehicle Vehicle(int? id)
+        {
+            return db.Vehicles.Find(id);
+        }
+
+        public IEnumerable<Vehicle> ParkedVehicles(ETypeVehicle vehicleType)
+        {
+            return GetAllVehicles().Where(p => p.VehicleType == vehicleType && p.ParkingSpot == null);
+        }
+
+        public void Add(Vehicle vehicle)
+        {
+            db.Vehicles.Add(vehicle);
+            SaveChanges();
+        }
+
+        public void Edit(Vehicle vehicle)
+        {
+            db.Entry(vehicle).State = EntityState.Modified;
+            SaveChanges();
+        }
+
+        public void CheckIn(int vehicleId, int parkingSpotId)
+        {
+            Vehicle vehicle = Vehicle(vehicleId);
+            vehicle.ParkingSpot = parkingSpotId;
+            Edit(vehicle);
+        }
+
+        public void CheckOut(int vehicleId)
+        {
+            Vehicle vehicle = Vehicle(vehicleId);
+            vehicle.ParkingSpot = null;
+            Edit(vehicle);
+        }
+
+        private void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+
         #region IDisposable Support
         private bool disposedValue = false; // To detect redundant calls
 
+        // This code added to correctly implement the disposable pattern.
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
@@ -33,7 +75,6 @@ namespace MVCGarage.Repositories
             }
         }
 
-        // This code added to correctly implement the disposable pattern.
         public void Dispose()
         {
             Dispose(true);
