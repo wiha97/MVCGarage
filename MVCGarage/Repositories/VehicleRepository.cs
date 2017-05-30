2969ec0f59f2271cfc2fb1b9fdd54cf2a012f7ae
Wille Hansson <wille.multi@live.se>
2017-05-30 11:51:32 +0200

    VR

--- MVCGarage/Repositories/VehicleRepository.cs
+++ MVCGarage/Repositories/VehicleRepository.cs
@@ -1,11 +1,51 @@
-﻿using System;
+﻿using MVCGarage.DataAccess;
+using MVCGarage.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace MVCGarage.Repositories
 {
-    public class VehicleRepository
+    public class VehicleRepository : IDisposable
     {
+        private GarageContext db = new GarageContext();
+
+        public IEnumerable<Vehicle> GetAllVehicles()
+        {
+            return db.Vehicles;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+
+
+
+
+
+
     }
 }
