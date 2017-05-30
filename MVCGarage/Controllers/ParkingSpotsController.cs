53afeffb90093498370ed30ce885597e4101d579
Liam44 <liam-b@orange.fr>
2017-05-30 10:27:20 +0200

    Controllers and repositories

---
+++ MVCGarage/Controllers/ParkingSpotsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCGarage.DataAccess;
+using MVCGarage.Models;
+
+namespace MVCGarage.Controllers
+{
+    public class ParkingSpotsController : Controller
+    {
+        private GarageContext db = new GarageContext();
+
+        // GET: ParkingSpots
+        public ActionResult Index()
+        {
+            return View(db.ParkingSpots.ToList());
+        }
+
+        // GET: ParkingSpots/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParkingSpot parkingSpot = db.ParkingSpots.Find(id);
+            if (parkingSpot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parkingSpot);
+        }
+
+        // GET: ParkingSpots/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ParkingSpots/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,VehicleID,Label,VehicleType")] ParkingSpot parkingSpot)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ParkingSpots.Add(parkingSpot);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(parkingSpot);
+        }
+
+        // GET: ParkingSpots/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParkingSpot parkingSpot = db.ParkingSpots.Find(id);
+            if (parkingSpot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parkingSpot);
+        }
+
+        // POST: ParkingSpots/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,VehicleID,Label,VehicleType")] ParkingSpot parkingSpot)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(parkingSpot).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(parkingSpot);
+        }
+
+        // GET: ParkingSpots/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParkingSpot parkingSpot = db.ParkingSpots.Find(id);
+            if (parkingSpot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parkingSpot);
+        }
+
+        // POST: ParkingSpots/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ParkingSpot parkingSpot = db.ParkingSpots.Find(id);
+            db.ParkingSpots.Remove(parkingSpot);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
