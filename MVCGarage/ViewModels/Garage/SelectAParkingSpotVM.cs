b0d47b16681dd118db3f263b97cbea6453958cfa
Liam44 <liam-b@orange.fr>
2017-05-31 11:25:21 +0200

    Conflicts

---
+++ MVCGarage/ViewModels/Garage/SelectAParkingSpotVM.cs
@@ -0,0 +1,19 @@
+﻿using MVCGarage.Models;
+using System.Collections.Generic;
+
+namespace MVCGarage.ViewModels.Garage
+{
+    public class SelectAParkingSpotVM
+    {
+        public int VehicleID { get; set; }
+        public Vehicle SelectedVehicle { get; set; }
+
+        public IEnumerable<ParkingSpot> ParkingSpots { get; set; }
+        public int ParkingSpotID { get; set; }
+
+        public string OriginActionName { get; set; }
+        public string OriginControllerName { get; set; }
+
+        public bool CheckInVehicle { get; set; }
+    }
+}
---
+++ MVCGarage/ViewModels/ParkingSpots/EditParkingSpotVM.cs
@@ -0,0 +1,10 @@
+﻿using MVCGarage.Models;
+
+namespace MVCGarage.ViewModels.ParkingSpots
+{
+    public class EditParkingSpotVM
+    {
+        public ParkingSpot ParkingSpot { get; set; }
+        public Vehicle ParkedVehicle { get; set; }
+    }
+}
